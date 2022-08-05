93d8a11ac90a0b93f2513a2b369c9ca62d5f3170
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2022-08-04 21:30:40 -0700

    Order imports with isort

--- custom_components/tekmar_482/__init__.py
+++ custom_components/tekmar_482/__init__.py
@@ -1,21 +1,13 @@
 """The Tekmar 482 Gateway Integration."""
 
-from . import hub
-
 import asyncio
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import (
-    CONF_NAME,
-    CONF_HOST,
-    CONF_PORT,
-    Platform,
-)
+from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT, Platform
 from homeassistant.core import HomeAssistant
-from .const import (
-    DOMAIN,
-    CONF_SETBACK_ENABLE
-)
+
+from . import hub
+from .const import CONF_SETBACK_ENABLE, DOMAIN
 
 PLATFORMS: list[str] = [Platform.SENSOR, Platform.CLIMATE, Platform.SELECT, Platform.SWITCH, Platform.BINARY_SENSOR, Platform.NUMBER]
 
